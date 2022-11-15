01275eda4248dd811840660a61a3c50b7348e924
Maripirs <mariapazmaluenda@gmail.com>
2022-11-15 13:16:20 -0800

    changed dek to go from 2 to 14, added shuffle function

--- app.js
+++ app.js
@@ -13,16 +13,32 @@ class Card{
     }
 }
 
+//copied from MDN Web Docs
+const randomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+  
 
 
 const defineDeck =() =>{
     for (let i = 0; i < suits.length; i++){
         for(let j = 0; j<13; j++){
-            let newCard = new Card (suits[i], j+1)
+            let newCard = new Card (suits[i], j+2)
             completeDeck.push(newCard)
         }
     }
 }
 
+const shuffleDeck = (deck) =>{
+    newDeck = [...deck]
+    while (newDeck.length >0){
+        let randomIndex= randomNum(0,newDeck.length-1)
+        gameDeck.push(newDeck[randomIndex]);
+        newDeck.splice(randomIndex,1)
+    }
+}
+
 defineDeck()
-console.log(completeDeck)
+shuffleDeck(completeDeck)
+// console.log(completeDeck)
+console.log(gameDeck)
